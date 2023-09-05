57812c35a7ac2a02e45af0a2e7a6ace6ef39b383
jjalixto <jjalixtoc@gmail.com>
2023-09-05 12:39:42 -0500

    avance-2

--- crud/controller/Cliente/Cliente.js
+++ crud/controller/Cliente/Cliente.js
@@ -1,38 +1,164 @@
-function datosClientesAll() {
-    // console.log("dentro de la funcion");
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', '../controller/Cliente/ClienteController.php', true);
+$(function () {
+    $("#task-result").hide();
+    fetchTask();
+    let edit = false;
 
-    xhttp.send();
-
-    xhttp.onreadystatechange = function () {
-
-        if (this.readyState == 4 && this.status == 200) {
-
-            let datos = JSON.parse(this.responseText);
-            // console.log(datos);
-            let res = document.querySelector('#res');
-            res.innerHTML = ''; //esto ayuda para que comienze en cero el
-
-            for (let item of datos) {
-                // console.log(item);
-                res.innerHTML += `
-
-            <tr>
-                <td>${item.id}</td>
-                <td>${item.nombre}</td>
-                <td>${item.apellido}</td>
-                <td>${item.tipo_doc}</td>
-                <td>${item.nro_doc}</td>
-                <td>${item.nro_tel_princ}</td>
-                <td>${item.email}</td>
-            </tr>
-            `
-            }
+    $("#search").keyup(() => {
+        if ($("#search").val()) {
+            let search = $("#search").val(); //se captura el valor
+            $.ajax({
+                url: "../controller/Cliente/ClienteController.php",
+                data: { search },
+                type: "POST",
+                success: function (response) {
+                    if (!response.error) {
+                        let tasks = JSON.parse(response);
+                        let template = ``;
+                        tasks.forEach(task => {
+                            template += `<li><a href="#" class="task-item">${task.nombre}</a></li>`
+                        });
+                        $("#task-result").show();
+                        $("#container").html(template);
+                    }
+                }
+            })
         }
-    }
-};
+    })
 
-window.onload = function () {
-    datosClientesAll();
-}
+
+    // function guardarDatos() {
+    $("#task-form").submit(e => {
+        e.preventDefault();
+        const postData = {
+            nombre: $("#nombre").val(), 
+            apellido: $("#apellido").val(),
+            tipo_doc: $("#tipo_doc").val(),
+            nro_doc: $("#nro_doc").val(),
+            nro_tel_princ: $("#nro_tel_princ").val(),
+            email: $("#email").val(),
+            id: $("#taskId").val(),
+        }
+        $.ajax({
+            url: "../controller/Cliente/ClienteController.php",
+            data: postData,
+            type: "POST",
+            success: function (response) {
+                if (!response.error) {
+                    fetchTask();
+                    $("#task-form").trigger("reset");
+                }
+            }
+        })
+    })
+    // }
+
+
+    function fetchTask() {
+        $.ajax({
+            url: "../controller/Cliente/ClienteController.php",
+            type: "GET",
+            success: function (response) {
+                const tasks = JSON.parse(response);
+
+                let template = ``;
+                tasks.forEach(task => {
+                    template += `
+                <tr taskId="${task.id}">
+                    <td>${task.id}</td>
+                    <td>${task.nombre}</td>
+                    <td>${task.apellido}</td>
+                    <td>${task.tipo_doc}</td>
+                    <td>${task.nro_doc}</td>
+                    <td>${task.nro_tel_princ}</td>
+                    <td>${task.email}</td>
+                    <td class = "d-flex ">
+                        <button class="btn btn-danger task-delete">Eliminar</button>
+                        <button id="task-item" class="btn btn-warning ">Modificar</button>
+                    </td>
+                </tr>
+                    `;
+                })
+                $("#tasks").html(template);
+            }
+        })
+    }
+
+    $(document).on("click", ".task-delete", () => {
+        if (confirm("Seguro que quieres eliminar este cliente")) {
+            const element = $(this)[0].activeElement.parentElement.parentElement;
+            const id = $(element).attr("taskId");
+            $.post("../controller/Cliente/ClienteController.php", { id }, () => {
+                fetchTask();
+            })
+        }
+    })
+
+    $(document).on("click", "#task-item", () => {
+        const element = $(this)[0].activeElement.parentElement.parentElement;
+        const id = $(element).attr("taskId");
+        let url = "../controller/Cliente/ClienteController.php";
+        $.ajax({
+            url:url,
+            data: {id},
+            type: "POST",
+            success:function(response){
+                if(!response.error){
+                    const task = JSON.parse(response);
+                    $("#nombre").val(task.nombre)
+                    $("#apellido").val(task.apellido)
+                    $("#tipo_doc").val(task.tipo_doc)
+                    $("#nro_doc").val(task.nro_doc)
+                    $("#nro_tel_princ").val(task.nro_tel_princ)
+                    $("#email").val(task.email)
+                    $("#taskId").val(task.id)
+                    edit = true;
+                }
+            }
+        })
+    })
+
+})
+
+
+
+    // }
+    // window.onload = function () {
+
+    //     buscarClientes();
+
+    // }
+// function datosClientesAll() {
+//     // console.log("dentro de la funcion");
+//     const xhttp = new XMLHttpRequest();
+//     xhttp.open('GET', '../controller/Cliente/ClienteController.php', true);
+
+//     xhttp.send();
+
+//     xhttp.onreadystatechange = function () {
+
+//         if (this.readyState == 4 && this.status == 200) {
+
+//             let datos = JSON.parse(this.responseText);
+//             // console.log(datos);
+//             let res = document.querySelector('#res');
+//             res.innerHTML = ''; //esto ayuda para que comienze en cero el
+
+//             for (let item of datos) {
+//                 // console.log(item);
+//                 res.innerHTML += `
+
+//             <tr>
+//                 <td>${item.id}</td>
+//                 <td>${item.nombre}</td>
+//                 <td>${item.apellido}</td>
+//                 <td>${item.tipo_doc}</td>
+//                 <td>${item.nro_doc}</td>
+//                 <td>${item.nro_tel_princ}</td>
+//                 <td>${item.email}</td>
+//             </tr>
+//             `
+//             }
+//         }
+//     }
+// };
+
--- crud/controller/Cliente/ClienteController.php
+++ crud/controller/Cliente/ClienteController.php
@@ -1,16 +1,144 @@
-
 <?php
 
-$conexion = pg_connect("host=localhost port=5432 dbname=supermercado user=postgres password=root");
+include "../../model/ModeloBase.php";
 
-$sql = "SELECT * FROM clientes";
+// --------------------buscar datos --------------------------------------------
+// $search = $_POST["search"];
 
-$result = pg_query($conexion, $sql);
+if(!empty($search)){ // negacion, empty vacia, si no esta vacia
+    $query = "SELECT * FROM clientes WHERE nombre LIKE '$search%'";  //el like es para que me muestre las coincidencias
+    $result = pg_query($conexion,$query);
 
-$conteo = pg_fetch_all($result);
+    if(!$result){
+        die("error en la consulta".pg_last_error($conexion));
+    }
 
-$convertir = json_encode($conteo);
+    $json = array();
 
-echo $convertir;
+    //array
+    while($row = pg_fetch_array($result)){
+        $json[] = array(
+            "id" => $row["id"],
+            "nombre" => $row["nombre"],
+            "apellidos" => $row["apellido"],
+            "tipo_doc" => $row["tipo_doc"],
+            "nro_doc" => $row["nro_doc"],
+            "nro_tel_princ" => $row["nro_tel_princ"],
+            "email" => $row["email"]
+        );
+    }
+
+    //aqui se transforma a un objeto json
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
+}
+
+// --------------------insertar datos  --------------------------------------------
+
+if(isset($_POST["nombre"])){
+    $task_nombre = $_POST["nombre"];
+    $task_apellido = $_POST["apellido"];
+    $task_tipo_doc = $_POST["tipo_doc"];
+    $task_nro_doc = $_POST["nro_doc"];
+    $task_nro_tel_princ = $_POST["nro_tel_princ"];
+    $task_email = $_POST["email"];
+
+    $query = "INSERT INTO clientes(nombre,apellido,tipo_doc,nro_doc,nro_tel_princ,email) VALUES('$task_nombre','$task_apellido','$task_tipo_doc','$task_nro_doc','$task_nro_tel_princ','$task_email')";
+    $result = pg_query($conexion,$query);
+
+    if(!$result){
+        die("error en la consulta".pg_last_error($conexion));
+    }
+    echo "tarea agregada";
+}
+
+
+// --------------------mostrar datos  --------------------------------------------
+
+$allquery = "SELECT * FROM clientes";
+$resultadoAll = pg_query($conexion,$allquery);
+
+if(!$resultadoAll){
+    die("hubo un error en la consulta".pg_last_error($conexion));
+}
+
+$json = array();
+
+//array
+while($row = pg_fetch_array($resultadoAll)){
+    $json[] = array(
+        "id" => $row["id"],
+        "nombre" => $row["nombre"],
+        "apellido" => $row["apellido"],
+        "tipo_doc" => $row["tipo_doc"],
+        "nro_doc" => $row["nro_doc"],
+        "nro_tel_princ" => $row["nro_tel_princ"],
+        "email" => $row["email"]
+    );
+}
+
+//aqui se transforma a un objeto json
+$jsonstring = json_encode($json);
+echo $jsonstring;
+
+
+//----------------eliminar cliente------------------------
+
+if(isset($_POST["id"])){
+    $id = $_POST["id"];
+    $consulta_id ="DELETE FROM clientes WHERE id = '$id'";
+    $result_id = pg_query($conexion,$consulta_id);
+
+    if(!$resultadoAll){
+        die("hubo un error en la consulta".pg_last_error($conexion));
+    }
+
+    echo "la tarea ha sido eliminada";
+}
+
+
+//---------------obtener datos de un cliente-----------------------
+
+if(isset($_POST["id"])){ // negacion, empty vacia, si no esta vacia
+
+    $id = $_POST["id"];
+    $query = "SELECT * FROM clientes WHERE id = {$id}";  //el like es para que me muestre las coincidencias
+    $result = pg_query($conexion,$query);
+
+    if(!$result){
+        die("error en la consulta".pg_last_error($conexion));
+    }
+
+    $json = array();
+
+    while($fila = pg_fetch_array($result)){
+        $json[] = array(
+            "id" => $fila["id"],
+            "nombre" => $fila["nombre"],
+            "apellido" => $fila["apellido"],
+            "tipo_doc" => $fila["tipo_doc"],
+            "nro_doc" => $fila["nro_doc"],
+            "nro_tel_princ" => $fila["nro_tel_princ"],
+            "email" => $fila["email"]
+        );
+    }
+
+    //aqui se transforma a un objeto json
+    $jsonstring = json_encode($json[0]);
+    echo $jsonstring;
+}
+
+
+// $conexion = pg_connect("host=localhost port=5432 dbname=supermercado user=postgres password=root");
+
+// $sql = "SELECT * FROM clientes";
+
+// $result = pg_query($conexion, $sql);
+
+// $conteo = pg_fetch_all($result);
+
+// $convertir = json_encode($conteo);
+
+// echo $convertir;
 
 ?>
